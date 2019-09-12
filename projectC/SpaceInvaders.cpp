55602e715fa1cb2c69ba56e93c5effac2c0bc6a4
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-09-12 14:15:51 -0300

    .

--- projectC/SpaceInvaders.cpp
+++ projectC/SpaceInvaders.cpp
@@ -8,40 +8,43 @@ struct space_component {
     int hp;
     bool isEnemy; // sefor inimigo, hp eh 1, se nao 3
     bool isSpaceShip; //se for falso eh um tiro
+    bool isVacuo;
 };
 
 space_component space[5][5];
-
 void buildSpace(){
     for(int i = 0; i < 5; i++){
         for(int j = 0; j < 5; j++){
             if (i < 2) {
-                createSpaceShip(i, j, 1, true, true);
+                createSpaceComponent(i, j, 1, true, true, false);
             } else if ( i == 4 && j == 2) {
-                createSpaceShip(i, j, 3, false, true);
-            }
+                createSpaceComponent(i, j, 3, false, true, false);
+            } else {
+                createSpaceComponent(i, j, 0, false, false, true);
+            } 
         }   
     }
 };
 
-void createSpaceShip(int i, int j, int hp, bool isEnemy, bool isSpaceShip) {
+void createSpaceComponent(int i, int j, int hp, bool isEnemy, bool isSpaceShip, bool isVacuo) {
     space_component component;
     component.hp          = hp;
     component.isEnemy     = isEnemy;
     component.isSpaceShip = isSpaceShip;
+    component.isVacuo     = isVacuo;
     space[i][j] = component;
 };
 
 
 string toString(space_component component) {
-    if(component.isSpaceShip && component.isEnemy){
+    if (component.isVacuo){
+        return "   ";    
+    } else if(component.isSpaceShip && component.isEnemy){
         return " V ";
     } else if (component.isSpaceShip && !component.isEnemy) {
         return " A ";
     } else if (!component.isSpaceShip && component.isEnemy) {
         return " v ";
-    } else if (component.hp == 0) {
-        return "   ";
     } else if (!component.isSpaceShip && !component.isEnemy) {
         return " ^ ";
     }
@@ -56,28 +59,36 @@ int getPlayerPosition(){
 };
 
 void move(bool left){
-    if (!left && (getPlayerPosition() != 4)){
-        space[4]
+    space_component playerSpaceShip = space[4][getPlayerPosition()];
+    
+    for (int j = 0; j < 5; j++){
+        if (!left && (getPlayerPosition() != 4)) {
+            space[4][j] = createSpaceComponent(4, j, false, false, true);
+            space[4][getPlayerPosition() + 1] = playerSpaceShip;  
+        } else if (left && (getPlayerPosition() != 0)) {
+            space[4][j] = createSpaceComponent(4, j, false, false, true);
+            space[4][getPlayerPosition() - 1] = playerSpaceShip; 
+        } 
     }
-};
-
-void shot(){
-    for ()
-
 };
 
 void menu(){
     while (true){
+        string option;
         cout << "A = Ir para esquerda" << endl;
         cout << "D = Ir para direita" << endl;
         cout << "W = Atirar" << endl;
         cout << "S = Sair" << endl;
-        cin << option;
+        cin >> option;
+        bool left = true;
 
-    if (option == "W"){
-            
-    }
-
+        if (option == "A") {
+            move(left);            
+        } else if (option == "D") {
+            move(!left);
+        } else {
+            break;            
+        }
     }
 };
 
