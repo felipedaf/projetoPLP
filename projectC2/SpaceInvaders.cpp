d969b4be63b5ccfe8f37a8f330714aea0963cf47
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-09-13 14:07:44 -0300

    finalizado

--- projectC2/SpaceInvaders.cpp
+++ projectC2/SpaceInvaders.cpp
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <iostream>
 #include <string>
+#include <unistd.h>
+#include <thread>
+#include <cstdlib>
 
 using namespace std;
 
 struct space_component {
     int hp;
-    bool isEnemy; // sefor inimigo, hp eh 1, se nao 3
-    bool isSpaceShip; //se for falso eh um tiro
+    bool isEnemy; 
+    bool isSpaceShip; 
     bool isVacuo;
     bool isShot;
 };
@@ -31,7 +34,7 @@ void buildSpace(){
             if (i < 2) {
                 createSpaceComponent(i, j, 1, true, true, false, false); //inimiga
             } else if ( i == 29 && j == 15) {
-                createSpaceComponent(i, j, 3, false, true, false, false); //jogador
+                createSpaceComponent(i, j, 1, false, true, false, false); //jogador
             } else {
                 createSpaceComponent(i, j, 0, false, false, true, false); //vacuo
             } 
@@ -77,6 +80,24 @@ int moveShot(){
         }   
     }
 };
+//bool isEnemy, bool isSpaceShip, bool isVacuo, bool isShot
+void moveEnemyShot(){
+    for(int i = 29; i>=0; i--){
+        for(int j = 29; j>=0; j--){
+            if(space[i][j].isShot && space[i][j].isEnemy){
+                if(space[i+1][j].isSpaceShip && !space[i+1][j].isEnemy){
+                     createSpaceComponent(i, j, 0, false, false, true, false); //colocando o vacuo         e retira a nave    
+                     createSpaceComponent(i+1, j, 0, false, false, true, false); //colocando o vacuo         e retira a nave    
+                     createSpaceComponent(i+1, j, 0, false, true, false, false);                             
+                     }
+                else{
+                    createSpaceComponent(i ,j ,0 ,false ,false ,true ,false);
+                    createSpaceComponent(i+1, j, 1, true, false, false, true);
+                }
+            }
+        }
+    }
+}
 
 void moveSpaceShip(bool left){
     int hpPlayerShip = space[29][getShipPosition()].hp;
@@ -108,6 +129,15 @@ void shot(){
     createSpaceComponent(28, positionPlayerShip, 2, false, false, false, true); //tiro           
 }
 
+void enemyShot(){
+    int enemy = rand()%29;
+    int i = 2;
+    if(space[i][enemy].isEnemy && space[i][enemy].isSpaceShip){
+        createSpaceComponent(i+1, enemy, 7, true, false, false, true);
+    }else if(space[i-1][enemy].isEnemy && space[i-1][enemy].isSpaceShip){
+        createSpaceComponent(i+1, enemy, 7, true, false, false, true);
+    }
+}
 void menu(){
     while (true){
         system("clear");
@@ -117,26 +147,58 @@ void menu(){
         cout << "d = Ir para direita" << endl;
         cout << "w = Atirar" << endl;
         cout << "s = Sair" << endl;
-        cin >> option;
+        cin  >> option;
         bool left = true;
 
         if (option == "a") {
-            moveShot();
             moveSpaceShip(left);            
+            moveEnemyShot();
+            moveShot();
+
         } else if (option == "d") {
+                moveEnemyShot();
             moveShot();
             moveSpaceShip(!left);
         } else if (option == "w") {
+            moveEnemyShot();
             moveShot();
             shot();
         } else if (option == "s") {
             break;
         }
+        enemyShot();
+        
     }
 };
 
+/*
+void nextFrame(){
+    while (true)
+    {
+        system("clear");
+        printGame();
+        cout << "a = Ir para esquerda" << endl;
+        cout << "d = Ir para direita" << endl;
+        cout << "w = Atirar" << endl;
+        cout << "s = Sair" << endl;
+        
+        moveEnemyShot();
+        moveShot();
+        
+        usleep(600000);
+    }
+}*/
+
 int main(){
     buildSpace();
-    menu();
+    thread t1(menu);
+    //thread t2(nextFrame);
+    if(t1.joinable()){
+        t1.join();
+    }
+    //if(t2.joinable()){
+      //  t2.join();
+    //}
+    
     printGame();
 };
