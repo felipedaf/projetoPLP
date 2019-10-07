86dc6e70104b349a54440c42bc197719d249abe1
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2019-10-06 21:51:36 -0300

    build array interface

--- projetoHS/SpaceInvaders.hs
+++ projetoHS/SpaceInvaders.hs
@@ -1,46 +1,83 @@
-data SpaceComponent = SpaceComponent {
-	isEnemy :: Bool,
-	isVacuo :: Bool,
-	isShot :: Bool
-}
-type SpaceMatrix = [[SpaceComponent]]
+data SpaceComponent = SpaceComponent  {
+    isPlayer :: Bool,
+    isEnemy :: Bool,
+    isVacuo :: Bool,
+    isShot :: Bool
+} deriving (Show, Eq)
 
 generateEnemy :: SpaceComponent
 generateEnemy = SpaceComponent {
-	isEnemy = True,
+    isPlayer = False,
+    isEnemy = True,
 	isVacuo = False,
-	isShot = False
-}
+    isShot = False
+};
 
 generateEnemyShot :: SpaceComponent
 generateEnemyShot = SpaceComponent {
-	isEnemy = True,
+    isPlayer = False,
+    isEnemy = True,
 	isVacuo = False,
-	isShot = True
-}
+    isShot = True
+};
 
 generatePlayer :: SpaceComponent
 generatePlayer = SpaceComponent {
-	isEnemy = False,
+    isPlayer = True,
+    isEnemy = False,
 	isVacuo = False,
-	isShot = False
-}
+    isShot = False
+};
 
 generatePlayerShot :: SpaceComponent
 generatePlayerShot = SpaceComponent {
-	isEnemy = False,
+    isPlayer = True,
+    isEnemy = False,
 	isVacuo = False,
-	isShot = True
-}
+    isShot = True
+};
 
 generateVacuo :: SpaceComponent
 generateVacuo = SpaceComponent {
-	isEnemy = False,
+    isPlayer = False,
+    isEnemy = False,
 	isVacuo = True,
-	isShot = False
-}
+    isShot = False
+};
 
+--gera a representacao em string dos spaceComponents
+spaceComponentToString :: SpaceComponent -> String 
+spaceComponentToString (SpaceComponent {isPlayer = isPlayer, isEnemy = isEnemy, isVacuo = isVacuo, isShot = isShot})
+    | (isPlayer == False) && (isEnemy == True) && (isShot == False) && (isVacuo == False) = "V" --enemy    
+    | (isPlayer == False) && (isEnemy == True) && (isShot == True)  && (isVacuo == False) = "v" --enemyShot
+    | (isPlayer == True) && (isEnemy == False) && (isShot == False) && (isVacuo == False) = "A" --player    
+    | (isPlayer == True) && (isEnemy == False) && (isShot == True) && (isVacuo == False) = "o" --playerShot
+    | otherwise = " " --vacuo
+
+--gera a representacao em string do array (space) 
+spaceToString :: [[SpaceComponent]] -> String -> Int -> String
+spaceToString spaceArray string index
+    | index == 9 = string --se chegar na ultima linha, retorna a string final
+    | otherwise = spaceToString spaceArray (string ++ (spaceComponentToString ((spaceArray !! index) !! 0) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 1) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 2) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 3) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 4) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 5) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 6) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 7) 
+        ++ spaceComponentToString ((spaceArray !! index) !! 8)) ++ "\n") (index+1)
+
+-- gera o array (space) composto por spaceComponent
+buildSpace :: SpaceComponent -> SpaceComponent -> SpaceComponent -> [[SpaceComponent]]
+buildSpace enemy vacuo player = take 3 (repeat (take 9 (repeat enemy))) ++ take 5 (repeat (take 9 (repeat vacuo))) ++ [[vacuo, vacuo, vacuo, vacuo, player, vacuo, vacuo, vacuo, vacuo]]
 
 main :: IO ()
 main = do
-    b <- getLine
+    let enemy = generateEnemy
+    let player = generatePlayer
+    let vacuo = generateVacuo
+    let space = buildSpace enemy vacuo player
+    putStrLn (spaceToString space "" 0)
+
+    return ()
--- projetoHS/teste.hs
+++
@@ -1,7 +0,0 @@
---rowMaker1 :: Int -> Int
---rowMaker1 n k = [ n .. n+k-1 ] : rowMaker1 (n+k) k
-
---rowMaker2 :: Int -> Int
---rowMaker2 n k = [ n,n-1..n-k+1 ] : rowMaker2 (n-k) k
-
-data Matrix a
